1b76d753c9ea3d7a45047fb0bb1853fd76bd76c6
nourdine med souleymane <nourdine2177@gmail.com>
2025-06-03 10:16:55 +0000

    fde

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -4,17 +4,17 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Language Lines
+    | Lignes de Langue pour l'Authentification
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
+    | Les lignes de langue suivantes sont utilisées lors de l'authentification 
+    | pour divers messages que nous devons afficher à l'utilisateur. Vous êtes 
+    | libre de modifier ces lignes de langue selon les besoins de votre application.
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'password' => 'Le mot de passe fourni est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
 
 ];
